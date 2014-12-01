432ab8e61a0200189e5989eeadb790507bf21c07
Pierig Le Saux <lesaux@pythian.com>
2014-12-01 12:49:39 -0500

    Update prereqs.pp

--- manifests/prereqs.pp
+++ manifests/prereqs.pp
@@ -8,7 +8,7 @@ class redis::prereqs {
       ensure_packages ( 'build-essential')
     }
     'Fedora', 'RedHat', 'CentOS', 'OEL', 'OracleLinux', 'Amazon': {
-      ensure_packages( 'make','gcc','glibc-devel' )
+      ensure_packages( ['make','gcc','glibc-devel'] )
     }
     default: {
       fail('The module does not support this OS.')
